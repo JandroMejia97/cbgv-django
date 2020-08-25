926e236fd5f9d7fd43908ab74b86bfd63bd78773
Carlos Alejandro Hernández Mejía <alejandromejia2013.27@outlook.com>
2020-08-25 00:40:55 -0300

    Añadiendo vistas y plantillas

---
+++ apps/exampleApp/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+
+from . import views
+urlpatterns = [
+    path('', views.IndexView.as_view(), name='home'),
+    path('categorias/', views.CategoriaList.as_view(), name='categorias-list'),
+    path('categorias/add/', views.CategoriaCreate.as_view(), name='categorias-add'),
+    path('subcategorias/',views.SubCategoriaList.as_view(),name='subcategorias-list'),
+    path('subcategorias/add',views.SubCategoriaCreate.as_view(),name='subcategorias-add'),
+    path('categoria/<pk>/', views.CategoriaDetail.as_view(),name="categoria-detail"),
+
+]
---
+++ apps/exampleApp/views.py
@@ -0,0 +1,46 @@
+from django.shortcuts import render
+
+from django.views import generic
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+
+from .models import *
+
+class SubCategoriaList(generic.ListView):
+    model = SubCategoria
+    context_object_name= 'subcategorias'
+
+
+class SubCategoriaCreate(generic.CreateView):
+    model = SubCategoria
+    fields = '__all__'
+    slug_field = 'nombre'
+    success_url = reverse_lazy('example:subcategorias-list')
+
+
+class CategoriaDetail(generic.DetailView):
+    context_object_name = 'categoria'
+    model = Categoria
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super().get_context_data(**kwargs)
+        # Add in a QuerySet of all the books
+        if self.object:
+            context['subcategorias'] = SubCategoria.objects.filter(categoria=self.object)      
+        return context
+    
+
+class CategoriaList(generic.ListView):
+    model = Categoria
+    context_object_name = 'categorias'
+
+
+class CategoriaCreate(generic.CreateView):
+    model = Categoria
+    fields = '__all__'
+    success_url = reverse_lazy('example:categorias-list')
+
+
+class IndexView(LoginRequiredMixin, generic.TemplateView):
+    template_name ='./index.html'
