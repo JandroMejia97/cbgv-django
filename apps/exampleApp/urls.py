b5a13516193e31f8f6f096d92cf913957594621e
Carlos Alejandro Hernández Mejía <alejandromejia2013.27@outlook.com>
2020-08-26 16:02:21 -0300

    Acciones: * Se añadieron las vistas de actualización y confirmación de borrado a la Categoría

--- apps/exampleApp/urls.py
+++ apps/exampleApp/urls.py
@@ -1,13 +1,16 @@
 from django.urls import path, include
 
 from . import views
+
 urlpatterns = [
     path('', views.IndexView.as_view(), name='home'),
     path('categorias/', views.CategoriaList.as_view(), name='categorias-list'),
     path('categorias/add/', views.CategoriaCreate.as_view(), name='categorias-add'),
+    path('categorias/<pk>/', views.CategoriaDetail.as_view(),name="categorias-detail"),
+    path('categorias/<pk>/update/',views.CategoriaUpdate.as_view(),name="categorias-update"),
+    path('categorias/<pk>/delete/', views.CategoriaDelete.as_view(), name='categorias-delete'),
     path('subcategorias/',views.SubCategoriaList.as_view(),name='subcategorias-list'),
     path('subcategorias/add',views.SubCategoriaCreate.as_view(),name='subcategorias-add'),
-    path('categoria/<pk>/', views.CategoriaDetail.as_view(),name="categoria-detail"),
-    path('categoria/update/<pk>',views.CategoriaUpdate.as_view(),name="categoria-update")
+    
 
 ]
